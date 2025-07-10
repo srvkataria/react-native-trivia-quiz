87f68f98d26d6d092158bf278815e22214c7c3cd
Saurabh <srvkataria@outlook.com>
2025-07-10 12:54:19 +0530

    Add questions, images

--- data/questionImageMap.tsx
+++ data/questionImageMap.tsx
@@ -2,5 +2,13 @@ import { ImageSourcePropType } from 'react-native';
 
 export const questionImages: { [key: string]: ImageSourcePropType } = {
   "1": require('@/assets/images/quiz/Ques001.png'),
-  "2": require('@/assets/images/quiz/Ques001.png')
+  "2": require('@/assets/images/quiz/Ques002.png'),
+  "3": require('@/assets/images/quiz/Ques003.png'),
+  "4": require('@/assets/images/quiz/Ques004.png'),
+  "5": require('@/assets/images/quiz/Ques005.png'),
+  "6": require('@/assets/images/quiz/Ques006.png'),
+  "7": require('@/assets/images/quiz/Ques007.png'),
+  "8": require('@/assets/images/quiz/Ques008.png'),
+  "9": require('@/assets/images/quiz/Ques009.png'),
+  "10": require('@/assets/images/quiz/Ques010.png')
 };
